55acdb5d7fb8a3fc81d82e9835456112164fe5b2
bobsshop <93291119+bobsshop@users.noreply.github.com>
2022-04-11 13:49:50 -0400

    js updated

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-const answer = document.querySelector("#answers");
-const answerCorrect = document.querySelector("#answers-correct");
-
 document.addEventListener("click", incorrectAnswer);
 document.addEventListener("click", correctAnswer);
 
 function incorrectAnswer() {
-    document.answer.style.backgroundColor = "red";
+    document.querySelector("#answers").style.backgroundColor = "red";
 }
 
 function correctAnswer() {
-    document.answerCorrect.style.backgroundColor = "green"
+    document.querySelector("#answers-correct").style.backgroundColor = "green";
 }
