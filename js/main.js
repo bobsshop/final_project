2b0d2a39c7c9981f018353eefe19ed1b74fed5e7
bobsshop <93291119+bobsshop@users.noreply.github.com>
2022-04-11 14:51:42 -0400

    changes

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ const incorrect1 = document.getElementById("incorrect1")
 const incorrect2 = document.getElementById("incorrect2")
 const incorrect3 = document.getElementById("incorrect3")
 const correct = document.getElementById("correct-answer")
+const reset = document.getElementById("mc-container")
 
 incorrect1.addEventListener("click", incorrectAnswer1);
 incorrect2.addEventListener("click", incorrectAnswer2);
 incorrect3.addEventListener("click", incorrectAnswer3);
 correct.addEventListener("click", correctAnswer);
+reset.addEventListener("click", resetQuestions)
 
 function incorrectAnswer1() {
     incorrect1.style.backgroundColor = "red";
@@ -22,5 +24,4 @@ function incorrectAnswer3() {
 
 function correctAnswer() {
     correct.style.backgroundColor = "green";
-    console.log("correct answer selected")
 }
