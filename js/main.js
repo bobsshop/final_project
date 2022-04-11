f4686cf0c5dc89f343823e1550deaa3d0b43c994
bobsshop <93291119+bobsshop@users.noreply.github.com>
2022-04-11 14:30:17 -0400

    js functionality

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-document.addEventListener("click", incorrectAnswer);
-document.addEventListener("click", correctAnswer);
+const incorrect1 = document.getElementById("incorrect1")
+const incorrect2 = document.getElementById("incorrect2")
+const incorrect3 = document.getElementById("incorrect3")
+const correct = document.getElementById("correct-answer")
 
-function incorrectAnswer() {
-    document.getElementById("incorrect").style.backgroundColor = "red";
+incorrect1.addEventListener("click", incorrectAnswer1);
+incorrect2.addEventListener("click", incorrectAnswer2);
+incorrect3.addEventListener("click", incorrectAnswer3);
+correct.addEventListener("click", correctAnswer);
+
+function incorrectAnswer1() {
+    incorrect1.style.backgroundColor = "red";
+}
+
+function incorrectAnswer2() {
+    incorrect2.style.backgroundColor = "red";
+}
+
+function incorrectAnswer3() {
+    incorrect3.style.backgroundColor = "red";
 }
 
 function correctAnswer() {
-    document.getElementById("correct-answer").style.backgroundColor = "green";
+    correct.style.backgroundColor = "green";
 }
