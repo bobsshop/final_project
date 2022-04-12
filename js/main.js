25aee345d97b18ebeaf1077bb0590caecb18b256
bobsshop <93291119+bobsshop@users.noreply.github.com>
2022-04-12 11:33:07 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,8 @@ const incorrect1 = document.getElementById("incorrect1")
 const incorrect2 = document.getElementById("incorrect2")
 const incorrect3 = document.getElementById("incorrect3")
 const correct = document.getElementById("correct-answer")
-const
 
-    incorrect1.addEventListener("click", incorrectAnswer1);
+incorrect1.addEventListener("click", incorrectAnswer1);
 incorrect2.addEventListener("click", incorrectAnswer2);
 incorrect3.addEventListener("click", incorrectAnswer3);
 correct.addEventListener("click", correctAnswer);
